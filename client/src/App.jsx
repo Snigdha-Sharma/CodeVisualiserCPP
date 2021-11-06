d0fa0259746a5105ac701d2724e68078d5ab04c0
Vibhu Rastogi <rastogivibhu1999@gmail.com>
2021-11-06 17:22:56 +0530

    basic website

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from '@mui/material/Button'
+
+const App = () =>{
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <Button variant="contained">Hello World</Button>
+        </div>
+    )
+}
+
+export default App;
