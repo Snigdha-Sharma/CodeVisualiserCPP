c92f02c577c2bbd08c319379a3afa2a19d030597
Vibhu Rastogi <rastogivibhu1999@gmail.com>
2021-11-14 20:33:43 +0530

    basic visualisation

--- client/src/Components/Visualisatin.jsx
+++ client/src/Components/Visualisatin.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import Button from '@mui/material/Button'
 import ButtonGroup from '@mui/material/ButtonGroup';
 import { styled } from '@mui/material/styles';
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 
@@ -15,15 +20,71 @@ const Item = styled(Paper)(({ theme }) => ({
     textAlign: 'left',
     color: theme.palette.text.secondary,
   }));
-
+const rows=[
+    [
+        {variable:'x' ,value: 0},
+        {variable:'y' ,value: 0}
+    ],
+    [
+        {variable:'x' ,value: 0},
+        {variable:'y' ,value: 1}
+    ],
+    [
+        {variable:'x' ,value: 10},
+        {variable:'y' ,value: 1}
+    ],
+    [
+        {variable:'x' ,value: 10},
+        {variable:'y' ,value: 12},
+        {variable:'z' ,value: '32, 24, 423, 1234, 3242'}
+    ]
+] 
 const Visualisatin = () => {
+    const [index,setIndex] = React.useState(0);
+
+    const onNext = () =>{
+        setIndex(index+1)
+    }
+
+    const onPrev = () =>{
+        setIndex(index-1)
+    }
+
     return (
         <div>
             <Item>visualistaion</Item>
-                    <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                        <Button>Previous Line</Button>
-                        <Button>Next Line</Button>
-                    </ButtonGroup>
+
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell>Variable</TableCell>
+                        <TableCell align="center">Value</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {rows[index].map((row) => (
+                        <TableRow
+                        key={row.variable}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                        <TableCell component="th" scope="row">
+                            {row.variable}
+                        </TableCell>
+                        <TableCell align="center">{row.value}</TableCell>
+                        
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+
+
+            <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                <Button onClick={onPrev}  >Previous Line</Button>
+                <Button onClick={onNext} >Next Line</Button>
+            </ButtonGroup>
         </div>
     )
 }
