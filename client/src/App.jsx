37c152a61b665578a27fbbfea90a4df0cbeb4788
Vibhu <rastogivibhu1999@gmail.com>
2021-11-21 19:00:24 +0530

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,18 +8,33 @@ import Visualisation from "./Components/Visualisation";
 
 const App = () =>{
     const [value, setValue] = React.useState('Enter C++ Code');
-
-    const rows=[[]]
+const [rows,setRows]= React.useState([[]])
 
     const handleChange = (event) => {
       setValue(event.target.value);
       console.log(value)
     };
     const onClickSubmit = () =>{
-        console.log('submit done');
+        // console.log('submit done');
+        const data= {"a": value}
+
+        fetch('http://localhost:3001',{
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'}
+    })
+        // setRows(rows1);
     }
     const onClickVis = () =>{
-        console.log('vis done');
+        // console.log('vis done');
+        fetch('http://localhost:3001',{
+       method: 'get',
+       headers: {'Content-Type':'application/json'}
+     }).then(response=>response.json())
+     .then(resp=>{
+       setRows(resp)
+     })
+
     }
     
     return (
